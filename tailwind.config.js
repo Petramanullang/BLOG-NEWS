f5acad4eb171cc273a79aa4945547610ddad4350
Petra Computer <izanami@192.168.1.11>
2023-10-05 16:11:39 +0700

    update

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-const withMT = require("@material-tailwind/html/utils/withMT");
-/** @type {import('tailwindcss').Config} */
-module.exports = withMT({
-  content: ["./index.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-});
